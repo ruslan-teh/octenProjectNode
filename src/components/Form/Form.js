115d8ccf4bbffda1a8640306d51c6048ea6b7da6
Ruslan Kovalisko <83656149+ruslan-teh@users.noreply.github.com>
2022-01-31 13:16:11 +0200

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -16,7 +16,7 @@ const Form = () => {
             <form onSubmit={handleSubmit(submit)}>
                 <label>Model: <input type="text" {...register('model')}/></label>
                 <label>Price: <input type="text" {...register('price')}/></label>
-                <label>Year: <input type="text" {...register('Year')}/></label>
+                <label>Year: <input type="text" {...register('year')}/></label>
                 <button>Send</button>
 
             </form>
